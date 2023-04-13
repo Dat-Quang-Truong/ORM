90f6fc2b8ece268ad3305b50c7cd87cecb5e7d7d
iamduc3024 <95302057+iamduc3024@users.noreply.github.com>
2023-04-14 01:54:31 +0700

    Add orm

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express();
+
+app.use(express.json());
+
+app.get('/customers', async (req, res) => {
+  const customer = await prisma.customers.findMany({
+    include: {
+      orders: true
+    },
+    where: {
+      customerNumber: 114
+    }
+  })
+  res.json(customer);
+})
+
+
+async function main() {
+  // const allcustomers = await prisma.customers.findMany({
+  //   include: {
+  //       orders: true
+  //   },
+    
+  // });
+  // console.log(allcustomers[1].orders);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
